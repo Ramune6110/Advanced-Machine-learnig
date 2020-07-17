8d97466fa5e93058ef4cc597d97b35654f362859
Ramune6110 <arduino6110@gmail.com>
2020-07-17 16:47:19 +0900

    [Fix]Adam

--- Adam.py
+++ Adam.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[18]:
+# In[5]:
 
 
 # requirement
@@ -22,14 +22,14 @@ class Adam:
         # 平均
         self.mu = np.array([[1],[2]])
         # 初期値
-        self.x_init = np.array([[3], [-1]])
+        self.w_init = np.array([[3], [-1]])
         # 正則化項係数
         self.lamda = 1.0
         # Adam parameter
-        self.b1 = 0.7
-        self.b2 = 0.99999
-        self.ee = 1.0e-8
-        self.aa = 0.2
+        self.beta1 = 0.7
+        self.beta2 = 0.99999
+        self.epsilon = 1.0e-8
+        self.eta = 0.2
         
     def calc_eval_fun(self):
         # 評価関数値の計算
@@ -39,55 +39,55 @@ class Adam:
                 self.Value[i, j] = np.dot(np.dot((w - self.mu).T, self.A), w - self.mu) + self.lamda * (np.abs(self.w_1[i]) + np.abs(self.w_2[j]))
 
     def proximal_operation(self, mu, q):
-        x_projection = np.zeros(mu.shape)
+        w_projection = np.zeros(mu.shape)
         for i in range(len(mu)):
             if mu[i] > q:
-                x_projection[i] = mu[i] - q
+                w_projection[i] = mu[i] - q
             else:
                 if np.abs(mu[i]) < q:
-                    x_projection[i] = 0
+                    w_projection[i] = 0
                 else:
-                    x_projection[i] = mu[i] + q
-        return x_projection
+                    w_projection[i] = mu[i] + q
+        return w_projection
     
     def main(self):
         # cvx
-        w_lasso = cv.Variable((2, 1))
-        J = cv.quad_form(w_lasso - self.mu, self.A) +  self.lamda * cv.norm(w_lasso, 1)
-        objective = cv.Minimize(J)
+        w_lasso     = cv.Variable((2, 1))
+        J           = cv.quad_form(w_lasso - self.mu, self.A) +  self.lamda * cv.norm(w_lasso, 1)
+        objective   = cv.Minimize(J)
         constraints = []
-        prob = cv.Problem(objective, constraints)
-        result = prob.solve(solver = cv.CVXOPT) 
-        w_lasso = w_lasso.value
+        prob        = cv.Problem(objective, constraints)
+        result      = prob.solve(solver = cv.CVXOPT) 
+        w_lasso     = w_lasso.value
 
         plt.contour(self.W1, self.W2, self.Value)
         
-        xt  = self.x_init
+        wt  = self.w_init
         # 値格納用配列
         x_history = []
         fvalues   = []
         g_history = []
-        M = np.zeros((2, 1))
-        V = np.zeros((2, 1))
+        mt = np.zeros((2, 1))
+        vt = np.zeros((2, 1))
         
         for t in range(1,101):
-            x_history.append(xt.T)
+            x_history.append(wt.T)
             # 勾配
-            grad = 2 * np.dot(self.A, xt - self.mu)
-            M = self.b1 * M + (1 - self.b1) * grad
-            V = self.b2 * V + (1 - self.b2) * (grad * grad)
-            Mest = M / (1 - self.b1 ** t)
-            Vest = V / (1 - self.b2 ** t)
+            grad  = 2 * np.dot(self.A, wt - self.mu)
+            mt    = self.beta1 * mt + (1 - self.beta1) * grad
+            vt    = self.beta2 * vt + (1 - self.beta2) * (grad * grad)
+            mthat = mt / (1 - self.beta1 ** t)
+            vthat = vt / (1 - self.beta2 ** t)
             
             g_history.append(grad.T)
             
-            rateProx = self.aa * np.ones((2, 1)) / (np.sqrt(Vest) + self.ee)
+            stepsize = self.eta * np.ones((2, 1)) / (np.sqrt(vthat) + self.epsilon)
             # 重みの更新式
-            xth = xt -  Mest * rateProx
-            xt  = np.array([self.proximal_operation(xth[0], self.lamda * rateProx[0]),
-                            self.proximal_operation(xth[1], self.lamda * rateProx[1])])
+            wth = wt -  mthat * stepsize
+            wt  = np.array([self.proximal_operation(wth[0], self.lamda * stepsize[0]),
+                            self.proximal_operation(wth[1], self.lamda * stepsize[1])])
             # 評価関数値の計算
-            J = np.dot(np.dot((xt - self.mu).T, self.A), (xt - self.mu)) + self.lamda * (np.abs(xt[0]) + np.abs(xt[1]))
+            J = np.dot(np.dot((wt - self.mu).T, self.A), (wt - self.mu)) + self.lamda * (np.abs(wt[0]) + np.abs(wt[1]))
             fvalues.append(J)
             
         fvalues   = np.vstack(fvalues)
